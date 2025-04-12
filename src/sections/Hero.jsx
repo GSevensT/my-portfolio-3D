a9f76b9a5f7e85289109638eb5ed7d26d90642db
GSevensT <143946090+GSevensT@users.noreply.github.com>
2025-04-13 07:06:13 +0800

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+const words = [
+    {text:'Ideas', imgPath: '/images/ideas.svg' }
+    {text:'Concepts', imgPath: '/images/concepts.svg' }
+]
 
 const Hero = () => {
   return (
@@ -7,7 +10,18 @@ const Hero = () => {
             <img src="/images/bg.png" alt="background"/>
         </div>
 
-        <div></div>
+        <div className="hero-layout">
+            {/*Left: HERO CONTENT*/}
+            <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5"></header>
+            <div className="flex flex-col gap-7">
+                <div className="hero-text">
+                    <h1>Shaping Designs</h1>
+                    <h1>into real projects</h1>
+                    <h1>that deliver results</h1>
+                </div>
+            </div>
+            {/*right: 3D model*/}
+        </div>
     </section>
   )
 }
