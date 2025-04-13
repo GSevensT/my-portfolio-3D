dd6552651ec61d5761080d753c10726e5a0f068c
GSevensT <143946090+GSevensT@users.noreply.github.com>
2025-04-13 10:23:22 +0800

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,7 @@
+
+
 const words = [
-    {text:'Ideas', imgPath: '/images/ideas.svg' }
+    {text:'Ideas', imgPath: '/images/ideas.svg' },
     {text:'Concepts', imgPath: '/images/concepts.svg' }
 ]
 
@@ -12,14 +14,32 @@ const Hero = () => {
 
         <div className="hero-layout">
             {/*Left: HERO CONTENT*/}
-            <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5"></header>
+            <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+
             <div className="flex flex-col gap-7">
                 <div className="hero-text">
-                    <h1>Shaping Designs</h1>
+                    <h1>Shaping 
+                        <span className="slide">
+                            <span className="wrapper">
+                                {words.map((word) =>(
+                                    <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                                        <img 
+                                        src={word.imgPath}
+                                        alt={word.text}
+                                        className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"/>
+
+                                        <span>{word.text}</span>
+                                    </span>
+                                ))}
+                            </span>
+                        </span>
+                    </h1>
                     <h1>into real projects</h1>
                     <h1>that deliver results</h1>
                 </div>
             </div>
+            </header>
+           
             {/*right: 3D model*/}
         </div>
     </section>
